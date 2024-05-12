09bd743768b8df8b83800d20ead6c5a151580980
Elham Fazel <55910811+fazelelham32@users.noreply.github.com>
2024-05-12 22:29:38 +0330

    Add files via upload

---
+++ Lesson7.py
@@ -0,0 +1,32 @@
+import sklearn
+from sklearn.utils import shuffle
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+
+data = pd.read_csv("F:/NEW/importent folder/main folder/projects for github/AI and AI in Python/codes and files/car.data")
+
+print(data.head())
+
+le = preprocessing.LabelEncoder()
+
+buying = le.fit_transform(list(data["buying"]))
+maint = le.fit_transform(list(data["maint"]))
+doors = le.fit_transform(list(data["doors"]))
+persons = le.fit_transform(list(data["persons"]))
+lug_boot = le.fit_transform(list(data["lug_boot"]))
+safety = le.fit_transform(list(data["safety"]))
+cls = le.fit_transform(list(data["class"]))
+
+x=list(zip(buying, maint, doors, persons, lug_boot, safety))  #features
+y=list(cls)  #labels
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x,y,test_size=0.1)
+
+#print(data.columns.tolist())
+
+#print(x_train)
+#print(x_test)
+print(y_train)
+#print(y_test)
